fa5e31376f2d5d2fc5bd779759cafec0324dfd65
Fabio Luciani <fabio.luciani127@educ.di.unito.it>
2022-04-11 16:24:33 +0200

    Tentativo mal riuscito di recuperare un albero

--- src/main.py
+++ src/main.py
@@ -1,19 +1,19 @@
 import random
 from potion import Potion
 from frame import Frame
-
+import spacy
+from nltk import Tree
 #funzione che restituisce una frase randomica da una lista di frasi
 def pick_random_start(phrases_list):
     return random.choice(phrases_list)
 
 '''da spostare'''
-import spacy
-from nltk import Tree
 
 
-nlp = spacy.load('en_core_web_sm')
 
-doc = nlp("The answer is Crisopa fly.") 
+nlp = spacy.load('it_core_news_sm')
+
+doc = nlp("Nella pozione polisucco sono presenti i sassi") 
 
 def to_nltk_tree(node):
     if node.n_lefts + node.n_rights > 0:
@@ -22,7 +22,13 @@ def to_nltk_tree(node):
         return node.orth_
 
 
-for token in doc:
-    print(token.text, token.pos_, token.dep_)
+#for token in doc:
+#    print(token.text, token.pos_, token.dep_)
+
+for sent in doc.sents:
+    t = to_nltk_tree(sent.root)
+
+print(t)
+
 
 [to_nltk_tree(sent.root).pretty_print() for sent in doc.sents]
