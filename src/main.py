1013d7760fb029b519864954c0741eccb390a552
Fabio Luciani <fabio.luciani127@educ.di.unito.it>
2022-04-17 17:23:01 +0200

    implemented pattern matching with dep. parsing

--- src/main.py
+++ src/main.py
@@ -79,10 +79,13 @@ answer2 = "the potion contains Crisopa fly"
 answer3 = "The first ingredient of the postion is the Crisopa fly"
 
 start2 = time.time()
-print(get_matched_patterns("aux_pattern", pattern, answer1))
+print(get_matched_patterns("aux_pattern", pattern, answer1)) 
 print(get_matched_patterns("aux_pattern", pattern, answer2))
-print(get_matched_patterns("aux_pattern", pattern, answer3)) 
+print(get_matched_patterns("aux_pattern", pattern, answer3))
 end2 = time.time()
+
+
+
 print(f"metodo 2 ci mette {end2 - start2} secondi :)")
 
 
@@ -92,7 +95,105 @@ così il match è più facile e si ottengono informazioni belle
 '''
 
 
+'''----------------------------------------------------------------------------------------------------------------------'''
+#* Alternativa con dependency matcher per riconoscere il pattern
 
+
+#*DISCLAIMER: i nomi dei pattern e dei vari attributi sono opinabili, lo so. Sentitevi liberi di cambiarli.
+
+from spacy.matcher import DependencyMatcher
+
+#TODO: riuscire a passare in input una lista di patter (?) e magari una lista di nomi di pattern associati 
+#TODO: (in modo da riuscire a capire quale pattern ha fatto match e avere il nome del pattern)
+def get_matched_patterns_from_dependency(name_pattern, pattern, text):
+    # funzione che usa il dependency matcher di spacy per riconoscere il pattern passato come parametro in un testo
+    # ordina gli elementi che fanno match col pattern e poi restituisce una lista di frasi che lo soddisfano
+    matched_elements = [] 
+    matcher = DependencyMatcher(nlp.vocab)
+    matcher.add(name_pattern, [pattern])
+    doc = nlp(text)
+    matches = matcher(doc)
+    matches.sort(key = lambda x : x[1])
+    #! Davvero scritta male, secondo me si può fare di meglio qui
+    for match in matches:
+        if match != []:
+            match_words = match[1]
+            if match_words[0] > match_words[1]: #! A volte vengono restituiti due indici, a volte 3, bisogna capire come restituire le parole in maniera decente
+                matched_elements.append((nlp.vocab[matches[0][0]].text, doc[match_words[2]:match_words[0]][:2]))
+            else:
+                matched_elements.append((nlp.vocab[matches[0][0]].text, doc[match_words[0]:match_words[1]+1][1:]))#? doc[match_words[0] : match_words[1]+1] se non metto il +1 si mangia l'ultima parola ??
+
+    return matched_elements #*matched element sarà una lista di tuple del tipo: ("nome_pattern", "parte di frase riconosciuta nel patter")
+
+
+
+
+#TODO:aggiungere pattern per una risposta con soli ingredienti (Es. "Crisopa Fly", Es2. "Crisopa fly and Mazzei's moustache")
+pattern1 = [
+    {"RIGHT_ID": "attr",
+     "RIGHT_ATTRS": {"LEMMA" : {"IN" : ["be", "use", "need", "have"]}}
+    },
+    {"LEFT_ID": "attr",
+     "REL_OP": ">",
+     "RIGHT_ID": "ingredient",
+     "RIGHT_ATTRS": {"DEP": "attr"}
+    }  
+]
+#? Sembra funzionante su passivo
+passive_pattern = [
+    {"RIGHT_ID": "passive",
+     "RIGHT_ATTRS": {"LEMMA" : {"IN" : ["use", "need"]}}
+    },
+    {
+     "LEFT_ID": "passive",
+     "REL_OP": ">",
+     "RIGHT_ID": "ingredient1",
+     "RIGHT_ATTRS": {"DEP": "nsubjpass"}   
+    },
+    {
+     "LEFT_ID": "ingredient1",
+     "REL_OP": ">",
+     "RIGHT_ID": "ingredient2",
+     "RIGHT_ATTRS": {"DEP": {"IN": ["compound", "amod"]}}     
+    }
+]
+#!? Abbozzato, funziona per la frase con contains, ma probabilmente si può generalizzare
+pattern2 = [
+    {"RIGHT_ID": "contains",
+     "RIGHT_ATTRS": {"LEMMA" : {"IN" : ["contain"]}}
+    },
+    {
+     "LEFT_ID": "contains",
+     "REL_OP": ">",
+     "RIGHT_ID": "ingredient_1",
+     "RIGHT_ATTRS": {"DEP": "ccomp"}   
+    },
+    {"LEFT_ID": "contains",
+     "REL_OP": ">",
+     "RIGHT_ID": "ingredient_2",
+     "RIGHT_ATTRS": {"DEP": "dobj"}
+    }
+]
+
+answer1 = "the answer is Crisopa Fly..." #pattern 1 OK
+answer2 = "the potion contains Crisopa fly" #pattern 3 OK
+answer3 = "The first ingredient of the potion is the Crisopa fly" #pattern 1 OK
+answer4 = "Crisopa fly is used in the potion" #pattern 2 OK
+answer5 = "Crisopa Fly is needed in the potion" #pattern 2 OK
+answer6 = "I think that Crisopa Fly is needed in the potion" #pattern 2 OK
+
+start2 = time.time()
+print(get_matched_patterns_from_dependency("aux_pattern", pattern1, answer1)) 
+print(get_matched_patterns_from_dependency("aux_pattern", pattern2, answer2))
+print(get_matched_patterns_from_dependency("aux_pattern", pattern1, answer3))
+print(get_matched_patterns_from_dependency("aux_pattern", passive_pattern, answer4))
+print(get_matched_patterns_from_dependency("aux_pattern", passive_pattern, answer5))
+print(get_matched_patterns_from_dependency("aux_pattern", passive_pattern, answer6))
+end2 = time.time()
+
+
+
+print(f"metodo 2 ci mette {end2 - start2} secondi :)")
 
 
 '''
@@ -103,34 +204,4 @@ example_words = ["bambino","bambina","mosca","mosche", "Crisopa","crisopa", "cri
 for w in example_words:
     print(ps.stem(w))
 
-'''
-
-'''
-PROBABILMENTE INUTILE
-def to_nltk_tree(node):
-    if node.n_lefts + node.n_rights > 0:
-        return Tree(node.orth_, [to_nltk_tree(child) for child in node.children])
-    else:
-        return node.orth_
-
-def print_token(sentence):
-    for token in sentence:
-        print(token.text, token.pos_, token.dep_)
-
-def to_tree(sentence):
-    for sent in sentence.sents:
-        t = to_nltk_tree(sent.root)
-    return t
-
-def pretty_print_tree(sentence):
-    [to_nltk_tree(sent.root).pretty_print() for sent in sentence.sents]
-
-def profundity_research(tree):
-    if isinstance(tree, Tree):
-        print(tree.label())
-        for child in tree:
-            profundity_research(child)
-    else:
-        print(tree)
-    
 '''
