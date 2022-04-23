339dd685b69c356ca61d2652cb3efda993afabb2
Fabio Luciani <fabio.luciani127@educ.di.unito.it>
2022-04-23 14:51:46 +0200

    Rimosse classi answer e potions. Inutilizzate

--- src/answer.py
+++
@@ -1,7 +0,0 @@
-class Answer:
-    ans_1 = "the answer is Crisopa Fly..."
-    ans_2 = "the potion contains Crisopa fly"
-    ans_3 = "The first ingredient of the postion is the Crisopa fly"
-    ans_4 = "Crisopa fly is used in the potion"
-    ans_5 = "Crisopa Fly is needed in the potion"
-    ans_6 = "I think that Crisopa Fly is needed in the potion"
