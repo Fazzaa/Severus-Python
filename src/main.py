9ada46488b47020ec09d8ce7d9b7f90c01e9683f
Fabio Luciani <fabio.luciani127@educ.di.unito.it>
2022-04-30 20:28:36 +0200

    testing execution

--- src/main.py
+++ src/main.py
@@ -8,5 +8,19 @@ f = Frame()
 name = input(greetings())
 find_pattern_name(f, pattern_name, name)
 print(start_interview(f))
-print(ask_ingredients_be(f))
+print(f.get_mood())
+print(f.get_chances())
 
+
+while (len(f.get_student_ingredients()) < f.get_ingredients_number()) or f.get_chances() > 0:
+    answer = input(ask_ingredients_be(f))
+    if f.check_response(test_patterns(answer)[0].text):
+        print("Buono")
+    else:
+        print("Male")
+    print(test_patterns(answer))
+    
+if f.get_chances() == 0:
+    print("Figlio di puttana studia")
+else:
+    print("Sei l'orgoglio di Mazzei")
