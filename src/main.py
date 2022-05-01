760ad7b774206a14294055762845bad6c1294cda
Fabio Luciani <fabio.luciani127@educ.di.unito.it>
2022-05-01 16:34:52 +0200

    fix

--- src/main.py
+++ src/main.py
@@ -2,78 +2,22 @@ from frame import Frame
 from dialogmanager import *
 from patterns import *
 from qna import *
-<<<<<<< HEAD
-from audio import *
-
-=======
->>>>>>> c6df04a80be78193f39e7a14f308346b7bc802bd
 
 f = Frame()
-text_to_speech = input("do you want to enable text to speech? (y=1/n=0)")
 
-#? SPEECH RECOGNITION OFF 
-if text_to_speech == "0":
-
-    name = input(greetings())
-    find_pattern_name(f, pattern_name, name)
-    print(start_interview(f))
-    print(f.get_mood())
-    print(f.get_chances())
+name = input(greetings())
+find_pattern_name(f, pattern_name, name)
+print(start_interview(f))
+print(f.get_mood())
+print(f.get_chances())
 
 
-<<<<<<< HEAD
-    while (len(f.get_student_ingredients()) < f.get_ingredients_number()) and f.get_chances() > 0:
-        answer = input(ask_ingredients_be(f))
-        result = test_patterns(answer)
-        if result != None:
-            if f.check_response(result):
-                print("Buono")
-            else:
-                print("Male")
-                f.dec_chances()
-                print(test_patterns(answer))
-        else:
-            print("I don't understand you")
-        
-    if f.get_chances() == 0:
-        print("Figlio di puttana studia")
-    else:
-        print("Sei l'orgoglio di Mazzei")
-
-#? SPEECH RECOGNITION ON
-else:
-    print(greetings())
-    name = speech_recognition()
-    find_pattern_name(f, pattern_name, name)
-    print(start_interview(f))
-    print(f.get_mood())
-    print(f.get_chances())
-
-    while (len(f.get_student_ingredients()) < f.get_ingredients_number()) and f.get_chances() > 0:
-        print(ask_ingredients_be(f))
-        answer = speech_recognition()
-        result = test_patterns(answer)
-        if result != None:
-            if f.check_response(result):
-                print("Buono")
-            else:
-                print("Male")
-                f.dec_chances()
-                print(test_patterns(answer))
-        else:
-            print("I don't understand you")
-        
-    if f.get_chances() == 0:
-        print("Figlio di puttana studia")
-    else:
-        print("Sei l'orgoglio di Mazzei")
-=======
 while (len(f.get_student_ingredients()) < f.get_ingredients_number()) and f.get_chances() > 0:
     answer = input(ask_ingredients_be(f))
     result = test_patterns(answer)
     if result != None:
         if f.check_response(result):
-            if f.full_frame():
+            if not f.full_frame():
                 good_response()
         else:
             bad_response()
@@ -87,4 +31,3 @@ else:
     print("Sei l'orgoglio di Mazzei")
 
 print(similar("Crisopa Flies","Crisopa Fly"))
->>>>>>> c6df04a80be78193f39e7a14f308346b7bc802bd
