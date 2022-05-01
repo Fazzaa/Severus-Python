9ba951e198b896a515abe7aa63fb647c35653678
andreafancellu <andreafancellu123@gmail.com>
2022-05-01 16:46:03 +0200

    bad things only happen to heroes

--- src/main.py
+++ src/main.py
@@ -2,15 +2,19 @@ from frame import Frame
 from dialogmanager import *
 from patterns import *
 from qna import *
-<<<<<<< HEAD
 from audio import *
 
-=======
->>>>>>> 760ad7b774206a14294055762845bad6c1294cda
 
 f = Frame()
+speech = input("Do you want to use the vocal interface? (y=1/n=0)")
 
-<<<<<<< HEAD
+if speech == "0":
+    name = input(greetings())
+    find_pattern_name(f, pattern_name, name)
+    print(start_interview(f))
+    print(f.get_mood())
+    print(f.get_chances())
+    
     while (len(f.get_student_ingredients()) < f.get_ingredients_number()) and f.get_chances() > 0:
         answer = input(ask_ingredients_be(f))
         result = test_patterns(answer)
@@ -58,31 +62,3 @@ else:
         print("Figlio di puttana studia")
     else:
         print("Sei l'orgoglio di Mazzei")
-=======
-name = input(greetings())
-find_pattern_name(f, pattern_name, name)
-print(start_interview(f))
-print(f.get_mood())
-print(f.get_chances())
-
-
-while (len(f.get_student_ingredients()) < f.get_ingredients_number()) and f.get_chances() > 0:
-    answer = input(ask_ingredients_be(f))
-    result = test_patterns(answer)
-    if result != None:
-        if f.check_response(result):
-            if not f.full_frame():
-                good_response()
-        else:
-            bad_response()
-            f.dec_chances()
-    else:
-        print("I don't understand you!")
-        
-if f.get_chances() == 0:
-    print("Figlio di puttana studia")
-else:
-    print("Sei l'orgoglio di Mazzei")
-
-print(similar("Crisopa Flies","Crisopa Fly"))
->>>>>>> 760ad7b774206a14294055762845bad6c1294cda
