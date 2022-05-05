52b7f80415acb5d445ef83d88be22947d6d6cf3f
Fabio Luciani <fabio.luciani127@educ.di.unito.it>
2022-05-05 13:57:01 +0200

    modified sentence construction

--- src/qna.py
+++ src/qna.py
@@ -12,33 +12,40 @@ realiser = nlg.Realiser(lexicon)
 # L'idea è che il dialogue manager scelga quali di queste funzioni usare 
 # a seconda delle risposte dell'utente
 
-# frase di inizio
+#* OK
 def greetings():
-    subject = nlg_factory.createNounPhrase(pick_random(GREETINGS))
-    sentence = nlg_factory.createClause(subject)
+    s_1 = nlg_factory.createClause(pick_random(GREETINGS))
 
     verb = nlg_factory.createVerbPhrase("be")
-    subj = nlg_factory.createNounPhrase("name")
-    subj.addPreModifier("your")
-    sentence2 = nlg_factory.createClause(subj, verb)
-    sentence2.setFeature(nlg.Feature.INTERROGATIVE_TYPE, nlg.InterrogativeType.WHAT_OBJECT)
+    subj = nlg_factory.createNounPhrase("your", "name")
+    s_2 = nlg_factory.createClause(subj, verb)
+    s_2.setFeature(nlg.Feature.INTERROGATIVE_TYPE, nlg.InterrogativeType.WHAT_OBJECT)
     
-    output = realiser.realiseSentence(sentence)
-    output = f"{output} {realiser.realiseSentence(sentence2)}"
+    coord = nlg_factory.createCoordinatedPhrase()
+    coord.setConjunction(",")
+    coord.addCoordinate(s_1)
+    coord.addCoordinate(s_2)
+
+    output = realiser.realiseSentence(coord)
     return output
 
+#* OK
 def start_interview(frame):
-    s1 = nlg_factory.createSentence(f"Well Mr. {frame.get_student_name()}")
+    s_1 = nlg_factory.createClause(f"Well {frame.get_student_name()}")
 
-    verb = nlg_factory.createVerbPhrase("start")
-    verb.setFeature(nlg.Feature.PERSON, nlg.Person.SECOND)
-    verb.addPreModifier("Let's")
-    obj = nlg_factory.createNounPhrase("interview")
-    obj.addPreModifier("the")
-    s2 = nlg_factory.createClause(verb, obj)
+    subj_2 = nlg_factory.createNounPhrase("we")
+    verb_2 = nlg_factory.createVerbPhrase("start")
+    verb_2.setFeature(nlg.Feature.PERSON, nlg.Person.SECOND)
+    verb_2.setFeature(nlg.Feature.MODAL, "can")
+    obj_2 = nlg_factory.createNounPhrase("the", "interview")
+    s_2 = nlg_factory.createClause(subj_2, verb_2, obj_2)
 
-    output = realiser.realiseSentence(s1)
-    output = f"{output} {realiser.realiseSentence(s2)}"
+    coord = nlg_factory.createCoordinatedPhrase()
+    coord.setConjunction(",")
+    coord.addCoordinate(s_1)
+    coord.addCoordinate(s_2)
+
+    output = realiser.realiseSentence(coord)
     return output
 
 def ask_ingredients(f):
@@ -54,6 +61,7 @@ def ask_ingredients(f):
         
 #######QUESTIONS########
 
+#! non controllata
 # frase del tipo: "Which ingredients are in Polyjuice potion ingredient's list?"
 def ask_ingredients_be_0(frame):
     n = pick_random([1,2])
@@ -61,17 +69,19 @@ def ask_ingredients_be_0(frame):
         verb = nlg_factory.createVerbPhrase("be")
     if n == 2:
         verb = nlg_factory.createVerbPhrase("be")
-        verb.setFeature(nlg.Feature.FORM, nlg.Form.IMPERATIVE)
-        verb.addPreModifier("must")
+        verb.setFeature(nlg.Feature.MODAL, "must")
     subject = nlg_factory.createNounPhrase("ingredient")
     subject.setPlural(True)
     object = nlg_factory.createNounPhrase("list")
     object.addPreModifier("in " + f"{frame.get_potion_name()}" +  " ingredient's")
+    
     sentence = nlg_factory.createClause(subject, verb, object)
     sentence.setFeature(nlg.Feature.INTERROGATIVE_TYPE, nlg.InterrogativeType.WHAT_SUBJECT)
+    
     output = realiser.realiseSentence(sentence)
     return output
 
+#! non controllata
 # frase del tipo: "What does Polyjuice potion contain?"
 def ask_ingredient_contain_0(frame):
     verb = nlg_factory.createVerbPhrase("contain")
@@ -82,6 +92,7 @@ def ask_ingredient_contain_0(frame):
     output = realiser.realiseSentence(sentence)
     return output
 
+#! non controllata
 # frase del tipo: "What does Polyjuice potion else contain?"
 def ask_ingredient_contain_else(potion):
     verb = nlg_factory.createVerbPhrase("contain")
@@ -93,6 +104,8 @@ def ask_ingredient_contain_else(potion):
     output = realiser.realiseSentence(sentence)
     return output
 
+
+#! non controllata
 # frase del tipo: "Which ingredient between Crisopa fly and Murtlap's tentacle is in Polyjuice potion ingredient's list?"
 def ask_ingredient_between(potion, ingredient1, ingredient2):
     verb = nlg_factory.createVerbPhrase("be")
@@ -106,7 +119,7 @@ def ask_ingredient_between(potion, ingredient1, ingredient2):
     output = realiser.realiseSentence(sentence)
     return output
 
-# frase del tipo: "What does Polyjuice potion not contain at all?"
+#! non controllata
 def ask_not_contain(potion):
     sentence = nlg_factory.createClause()
     sentence.setSubject(f"{potion}")
@@ -120,8 +133,8 @@ def ask_not_contain(potion):
     return output
 
 ########ANSWERS########
-
-def bad_response(frame, ingredient):
+#?DA REVISIONARE ALCUNE FRASI
+def bad_response(frame):
     n = pick_random([1,2])
     if frame.get_mood() == 2:
         subj = nlg_factory.createNounPhrase("you")
@@ -132,77 +145,115 @@ def bad_response(frame, ingredient):
             sentence.setNegated(True)
         else:
             verb = nlg_factory.createVerbPhrase("be")   
-            obj = nlg_factory.createNounPhrase("stupid")
-            obj.addPreModifier("so")   
+            obj = nlg_factory.createNounPhrase("so", "stupid")
             sentence = nlg_factory.createClause(subj, verb, obj)
             sentence.setFeature(nlg.Feature.INTERROGATIVE_TYPE, nlg.InterrogativeType.HOW)
     elif frame.get_mood() == 1:
         if n == 1:
             verb = nlg_factory.createVerbPhrase("waste")
-            dobj = nlg_factory.createNounPhrase("time")
-            dobj.addPreModifier("my")
             verb.setNegated(True)
             verb.setFeature(nlg.Feature.PERSON, nlg.Person.SECOND)
+            dobj = nlg_factory.createNounPhrase("my", "time")
+            
             sentence = nlg_factory.createClause(verb, dobj)
         else:
             subj = nlg_factory.createNounPhrase("That")
             verb = nlg_factory.createVerbPhrase("be")
-            obj = nlg_factory.createNounPhrase("wrong")
-            obj.addPreModifier("totally")
+            obj = nlg_factory.createNounPhrase("totally", "wrong")
+
             sentence = nlg_factory.createClause(subj, verb, obj)
     else:
         if n == 1:
             verb =  nlg_factory.createVerbPhrase("try")
             verb.setFeature(nlg.Feature.PERSON, nlg.Person.SECOND)
             verb.addPostModifier("again")
+            
             sentence = nlg_factory.createClause(verb)
         else: 
             verb = nlg_factory.createVerbPhrase("be")
+            verb.setNegated(True)
             subj = nlg_factory.createNounPhrase("That")
-            obj = nlg_factory.createNounPhrase("ingredient")
-            obj.addPreModifier("the correct")
+            obj = nlg_factory.createNounPhrase("the correct", "ingredient")
+
             sentence = nlg_factory.createClause(subj, verb, obj)
-            sentence.setNegated(True)
             
     output = realiser.realiseSentence(sentence)
     return output
 
+#*CREDO OK
 def good_response(frame, ingredient):
     if frame.get_mood() == 2:    
-        return "decent"
+        verb_1 = nlg_factory.createVerbPhrase("be")
+        subj_1 = nlg_factory.createNounPhrase("That")
+        obj_1 = nlg_factory.createNounPhrase("slightly", "sufficient")
+        s_1 = nlg_factory.createClause(subj_1, verb_1, obj_1)
+
+        verb_2 = nlg_factory.createVerbPhrase("go on")
+        verb_2.setFeature(nlg.Feature.MODAL, "can")
+        subj_2 = nlg_factory.createNounPhrase("you")
+        s_2 = nlg_factory.createClause(subj_2, verb_2)
+
+        coord = nlg_factory.createCoordinatedPhrase()
+        coord.setConjunction(",")
+        coord.addCoordinate(s_1)
+        coord.addCoordinate(s_2)
+
+        output = realiser.realiseSentence(coord)
+        
     elif frame.get_mood() == 1:
-        return f"ok, {ingredient} is correct"
-    else:
-        return "great!"
+        s_1 = nlg_factory.createClause("Ok")
 
+        subj_2 = nlg_factory.createNounPhrase({ingredient})
+        verb_2 = nlg_factory.createVerbPhrase("be")
+        obj_2 = nlg_factory.createNounPhrase("correct")
+        s_2 = nlg_factory.createClause(subj_2, verb_2, obj_2)
 
-def valutation(frame, vote):
-    verb = nlg_factory.createVerbPhrase("go")
-    verb.setFeature(nlg.Feature.TENSE, nlg.Tense.PAST)
-    subj = nlg_factory.createNounPhrase("interview")
-    subj.addPreModifier(f"Well {frame.get_student_name()},")
-    subj.addPreModifier("the")
-    if vote < 18:
-        obj = nlg_factory.createNounPhrase("bad")
-        if vote == 0:
-            obj.addPreModifier("very")
-    elif vote >= 18 and vote <= 25:
-        obj = nlg_factory.createNounPhrase("well")
+        coord = nlg_factory.createCoordinatedPhrase()
+        coord.setConjunction(",")
+        coord.addCoordinate(s_1)
+        coord.addCoordinate(s_2)
+
+        output = realiser.realiseSentence(coord)
+        
     else:
-        obj = nlg_factory.createNounPhrase("well")
-        obj.addPreModifier("very")
-    sentence_1 = nlg_factory.createClause(subj, verb, obj)
-    output = realiser.realiseSentence(sentence_1)
-    
-    subj_2 = nlg_factory.createNounPhrase("vote")
-    subj_2.addPreModifier("Your")
-    verb_2 = nlg_factory.createVerbPhrase("be")
-    obj_2 = nlg_factory.createNounPhrase(str(vote))
-    sentence_2 = nlg_factory.createClause(subj_2, verb_2, obj_2)
-    
-    output += realiser.realiseSentence(sentence_2)
+        verb = nlg_factory.createVerbPhrase("be")
+        subj = nlg_factory.createNounPhrase("That")
+        obj = nlg_factory.createNounPhrase("the correct", "ingredient")
+        s_1 = nlg_factory.createClause(subj, verb, obj)
+
+        output = realiser.realiseSentence(s_1)
+
     return output
 
-frame = Frame()
-frame.add_potions()
-print(ask_ingredients_be_0(frame))
+#*OK
+def valutation(frame, vote):
+    s_1 = nlg_factory.createClause(f"Well {frame.get_student_name()}")
+
+    verb_2 = nlg_factory.createVerbPhrase("go")
+    verb_2.setFeature(nlg.Feature.TENSE, nlg.Tense.PAST)
+    subj_2 = nlg_factory.createNounPhrase("the", "interview")
+    
+    if vote == "Troll":
+        obj_2 = nlg_factory.createNounPhrase("very", "bad")
+    if vote == "Poor" or vote == "Dreadful":
+        obj_2 = nlg_factory.createNounPhrase("bad")
+    elif vote == "Acceptable" or vote == "Exceeds Expectations":
+        obj_2 = nlg_factory.createNounPhrase("well")
+    else:
+        obj_2 = nlg_factory.createNounPhrase("very", "well")
+
+    s_2 = nlg_factory.createClause(subj_2, verb_2, obj_2)
+    
+    subj_3 = nlg_factory.createNounPhrase("your", "vote")
+    verb_3 = nlg_factory.createVerbPhrase("be")
+    obj_3 = nlg_factory.createNounPhrase(vote)
+    s_3 = nlg_factory.createClause(subj_3, verb_3, obj_3)
+
+    coord = nlg_factory.createCoordinatedPhrase()
+    coord.addCoordinate(s_1)
+    coord.addCoordinate(s_2)
+    coord.addCoordinate(s_3)
+
+    output = realiser.realiseSentence(coord)
+    return output
+
