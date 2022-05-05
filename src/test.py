2c5951c9f9de047fec79d0c334d60c4380c2160c
PaoloBonicco7 <paolo.bonicco1@gmail.com>
2022-05-05 19:49:45 +0200

    fix test

--- src/test.py
+++ src/test.py
@@ -12,7 +12,7 @@ class TestFrameMethods(unittest.TestCase):
     def test_check_response(self):
         f = Frame()
         f.set_potion_name("Polyjuice potion")
-        self.assertTrue(f.check_response("Crisopa flies"))
+        self.assertTrue(f.check_response({"Crisopa flies"}))
 
 class TestDialogueManagerMethods(unittest.TestCase):
 
@@ -46,7 +46,39 @@ class TestDialogueManagerMethods(unittest.TestCase):
         answer = "my name is Andrea Fancellu"
         find_pattern_name(f, pattern_name, answer)
         self.assertEqual(f.get_student_name(), "Andrea Fancellu")
-        
+ 
+class TestPatterns(unittest.TestCase):
+    
+    def test_passive_pattern_common(self):
+        answer = "puffer fish eyes is used in the potion"
+        result = get_matched_patterns_from_dependency("passive_pattern_common", passive_pattern_common, answer)
+        self.assertEqual(result, "puffer fish eyes")
+
+    def test_passive_pattern_propn(self):
+        answer = "Puffer Fish Eyes is used in the potion"
+        result = get_matched_patterns_from_dependency("passive_pattern_propn", passive_pattern_propn, answer)
+        self.assertEqual(result, "Puffer Fish Eyes")
+
+    def test_passive_pattern_common_2(self):
+        answer = "Murtlap's tentacle is used in the potion"
+        result = get_matched_patterns_from_dependency("passive_pattern_common", passive_pattern_common, answer)
+        self.assertEqual(result, "Murtlap's tentacle")
+
+    def test_pattern_aux(self):
+        answer = "the answer is Crisopa Flies..."
+        result = get_matched_patterns_from_dependency("pattern_aux", pattern_aux, answer)
+        self.assertEqual(result, "Crisopa Flies")
+
+    def test_pattern_verb(self):
+        answer = "the potion contains Crisopa flies"
+        result = get_matched_patterns_from_dependency("pattern_verb", pattern_verb, answer)
+        self.assertEqual(result, "Crisopa flies")
+
+    def test_pattern_name(self):
+        f = Frame()
+        answer = "my name is Andrea Fancellu"
+        find_pattern_name(f, pattern_name, answer)
+        self.assertEqual(f.get_student_name(), "Andrea Fancellu")        
 
 if __name__ == '__main__':
     unittest.main()
