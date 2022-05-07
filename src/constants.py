8d7974ee68b74a0524513919f9187cb1314c020c
Fabio Luciani <fabio.luciani127@educ.di.unito.it>
2022-05-07 23:59:25 +0200

    added constants

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,7 @@
-GREETINGS=['Hi','Hello', 'Welcome']
+from patterns import *
 
+GREETINGS=['Hi','Hello', 'Welcome']
+PATTERNS_NAME = ["passive_pattern_2","pattern_verb_5", "pattern_verb_4","passive_pattern_propn", "passive_pattern_common", "pattern_verb","pattern_aux", "pattern_verb_2","passive_pattern", "pattern_verb_3"]
+PATTERNS = [passive_pattern_2, pattern_verb_5, pattern_verb_4,passive_pattern_propn, passive_pattern_common, pattern_verb, pattern_aux, pattern_verb_2, passive_pattern, pattern_verb_3]
+VOTE_DICT = {0: "Troll",1: "Troll",2: "Troll",3: "Dreadful",4: "Dreadful",5: "Poor",6: "Acceptable",
+                   7: "Acceptable",8: "Exceeds Expectations",9: "Exceeds Expectations",10: "Outstanding"}
