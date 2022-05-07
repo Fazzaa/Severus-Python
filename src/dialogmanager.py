021d449dc1d2e61a329fc0ea8fe51bb5331b733a
Fabio Luciani <fabio.luciani127@educ.di.unito.it>
2022-05-07 23:59:43 +0200

    little fix

--- src/dialogmanager.py
+++ src/dialogmanager.py
@@ -1,13 +1,8 @@
 import random
 import spacy
-from patterns import *
+from constants import *
 from spacy.matcher import DependencyMatcher
 from difflib import SequenceMatcher
-
-vote_dictionary = {0: "Troll",1: "Troll",2: "Troll",3: "Dreadful",4: "Dreadful",5: "Poor",6: "Acceptable",
-                   7: "Acceptable",8: "Exceeds Expectations",9: "Exceeds Expectations",10: "Outstanding"}
-patterns_name = ["passive_pattern_2","pattern_verb_5", "pattern_verb_4","passive_pattern_propn", "passive_pattern_common", "pattern_verb","pattern_aux", "pattern_verb_2","passive_pattern", "pattern_verb_3"]
-patterns = [passive_pattern_2, pattern_verb_5, pattern_verb_4,passive_pattern_propn, passive_pattern_common, pattern_verb, pattern_aux, pattern_verb_2, passive_pattern, pattern_verb_3]
 
 nlp = spacy.load('en_core_web_sm')
         
@@ -21,8 +16,6 @@ def get_longest(match_list):
             l = match
     return l
 
-
-
 def get_matched_patterns_from_dependency(name_pattern, pattern, text):
     matched_elements = [] 
     matcher = DependencyMatcher(nlp.vocab)
@@ -32,7 +25,6 @@ def get_matched_patterns_from_dependency(name_pattern, pattern, text):
     matches.sort(key = lambda x : x[1])
     for match in matches:
         match_words = sorted(match[1])
-        print(name_pattern)
         if name_pattern == "passive_pattern_propn" or name_pattern == "passive_pattern_common" or name_pattern == "passive_pattern" or name_pattern == "passive_pattern_2":
             matched_elements.append(doc[match_words[0]:match_words[len(match_words)-1]][:-1])
         elif name_pattern == "pattern_verb" or name_pattern == "pattern_aux" or name_pattern == "pattern_verb_2" or name_pattern == "pattern_verb_3" or name_pattern == "pattern_verb_4" or name_pattern == "pattern_verb_5":
@@ -60,8 +52,8 @@ def find_pattern_name(frame, pattern, text):
 
 def test_patterns(text):
     i = 0
-    while i < len(patterns):
-        result = get_matched_patterns_from_dependency(patterns_name[i], patterns[i], text)
+    while i < len(PATTERNS):
+        result = get_matched_patterns_from_dependency(PATTERNS_NAME[i], PATTERNS[i], text)
         if result != "No Match":
             result = result.split(",")
             return result
@@ -82,5 +74,5 @@ def get_vote(frame):
         if frame.get_mood() == 0 and n_vote >= 5:
             n_vote = 6
     
-    return vote_dictionary[n_vote]
+    return VOTE_DICT[n_vote]
 
