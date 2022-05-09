435c9403e26116e84d9f0b76825fc36405c3d021
Fabio Luciani <fabio.luciani127@educ.di.unito.it>
2022-05-09 21:46:10 +0200

    added list of constants

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,16 @@
 from patterns import *
 
 GREETINGS=['Hi','Hello', 'Welcome']
-PATTERNS_NAME = ["passive_pattern_2","pattern_verb_5", "pattern_verb_4","passive_pattern_propn", "passive_pattern_common", "pattern_verb","pattern_aux", "pattern_verb_2","passive_pattern", "pattern_verb_3"]
-PATTERNS = [passive_pattern_2, pattern_verb_5, pattern_verb_4,passive_pattern_propn, passive_pattern_common, pattern_verb, pattern_aux, pattern_verb_2, passive_pattern, pattern_verb_3]
+
+PATTERNS_NAME = ["passive_pattern_2", "passive_pattern_propn", "passive_pattern_common", "passive_pattern",
+                "pattern_verb_2", "pattern_verb_3", "pattern_verb_1","pattern_aux_2", "pattern_aux_1", "pattern_aux_0"]
+
+PATTERNS = [passive_pattern_2, passive_pattern_propn, passive_pattern_common, passive_pattern,
+            pattern_verb_2, pattern_verb_3, pattern_verb_1,pattern_aux_2, pattern_aux_1, pattern_aux_0]
+
+
+ACTIVE_PATTERNS = ["pattern_aux_2", "pattern_aux_1", "pattern_aux_0", "pattern_verb_2", "pattern_verb_3", "pattern_verb_1"]
+PASSIVE_PATTERNS = ["passive_pattern_2", "passive_pattern_propn", "passive_pattern_common", "passive_pattern"]
+
 VOTE_DICT = {0: "Troll",1: "Troll",2: "Troll",3: "Dreadful",4: "Dreadful",5: "Poor",6: "Acceptable",
                    7: "Acceptable",8: "Exceeds Expectations",9: "Exceeds Expectations",10: "Outstanding"}
