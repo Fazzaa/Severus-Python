fa870124d172d7e917dfd25ecd5455db721ad382
Fabio Luciani <lu.fa.98@gmail.com>
2022-06-03 18:31:28 +0200

    added constants

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
 from patterns import *
 
-GREETINGS=['Hi','Hello', 'Welcome']
+GREETINGS=['Hi', 'Hello', 'Welcome', "Good morning"]
 
 PATTERNS_NAME = ["passive_pattern_2", "passive_pattern_propn", "passive_pattern_common", "passive_pattern",
                 "pattern_verb_2", "pattern_verb_3", "pattern_verb_1","pattern_aux_2", "pattern_aux_1", "pattern_aux_0"]
@@ -14,3 +14,11 @@ PASSIVE_PATTERNS = ["passive_pattern_2", "passive_pattern_propn", "passive_patte
 
 VOTE_DICT = {0: "Troll",1: "Troll",2: "Troll",3: "Dreadful",4: "Dreadful",5: "Poor",6: "Acceptable",
                    7: "Acceptable",8: "Exceeds Expectations",9: "Exceeds Expectations",10: "Outstanding"}
+
+SIN_BESIDES = ["besides", "in addition to", "beyond", "plus", "further"]
+SIN_VERBS = ["contain", "use", "need", "enclose", "comprehend", "embody", "include"]
+SIN_LIST = ["list", "recipe"]
+SIN_INGREDIENT = ["ingredient", "element", "component"]
+SIN_POTION = ["potion", "mixture", "elixir"]
+SIN_INSULTS = ["stupid", "son of a pig", "dunderhead"]
+SIN_CORRECT = ["correct", "right", "good", "fair", "proper"]
